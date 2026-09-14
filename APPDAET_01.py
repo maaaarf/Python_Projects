ad5c2b09ce7ae803d54a03decb4364904e24b47f
Marc Andrei Bersabe <bersabemarcssi@gmail.com>
2026-09-14 15:48:27 +0800

    added first fike

---
+++ APPDAET_01.py
@@ -0,0 +1,17 @@
+#september 7, 2026
+
+print("Ready for Python!")
+
+print("Hello World")
+
+# variables
+name = "Oscar the Grouch"
+age = 51
+wealth = 222000444666.56
+is_wealthy = True
+
+print(type(name))
+print(type(age))
+print(type(wealth))
+print(type(is_wealthy))
+
